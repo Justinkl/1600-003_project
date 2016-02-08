35d8b0162b23a8be493898c79b12dd4fb22d7d20
unknown <10740244@CS514-05.ad.uvu.edu>
2016-02-08 09:32:44 -0700

    adding ifstatements

---
+++ DGM 1600/Assets/IfStatement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatement : MonoBehaviour
+{
+	public float Lemmingpopulation = 100.0f;
+	public float Toomanylemmings = 70.0f;
+	public float Lemmingsaredying = 30.0f;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			Lemmingtest ();
+
+		Lemmingpopulation -= Time.deltaTime * 5f;
+	}
+
+
+	void Lemmingtest ()
+	{
+		if (Lemmingpopulation > Toomanylemmings)
+		{
+			print ("Too many lemmings.");
+		} 
+		else if (Lemmingpopulation < Lemmingsaredying) 
+		{
+			print ("Lemmings are dying.");
+		}
+		else
+		{
+			print ("Just enough Lemmings");
+		}
+	
+	}
+}
